938b27bc6212efc066115d026a17eb26c6b2f59f
CryptoSalamander <dhy03196@naver.com>
2020-04-27 19:59:48 +0900

    [ADD]Sort.go

--- Sort.go
+++ Sort.go
@@ -1,21 +1,22 @@
 package main
+
 import "sort"
 import "fmt"
 
 type byLength []string
 
 func (s byLength) Len() int {
-    return len(s)
+	return len(s)
 }
 func (s byLength) Swap(i, j int) {
-    s[i], s[j] = s[j], s[i]
+	s[i], s[j] = s[j], s[i]
 }
 func (s byLength) Less(i, j int) bool {
-    return len(s[i]) < len(s[j])
+	return len(s[i]) < len(s[j])
 }
 
 func main() {
-    fruits := []string{"peach", "banana", "kiwi"}
-    sort.Sort(byLength(fruits))
-    fmt.Println(fruits)
-  }
+	fruits := []string{"peach", "banana", "kiwi"}
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
+}
